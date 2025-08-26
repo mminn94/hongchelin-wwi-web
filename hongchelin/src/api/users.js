9d51021313409802e89a536bbe474d3cb3e3e3b9
[mminn94] <[joykim0922@gmail.com]>
2025-08-26 22:20:04 +0900

    [CM]_오류관리

--- hongchelin/src/api/users.js
+++ hongchelin/src/api/users.js
@@ -24,12 +24,14 @@ export const updateMyNickname = async (nickname) => {
   return res.data;
 };
 
-export const updateMyProfileImage = async (file) => {
-  const formData = new FormData();
-  formData.append("file", file);
-  const res = await api.post(join(ME_PATH, "profile-image"), formData);
+export async function updateMyProfileImage(file) {
+  const form = new FormData();
+  form.append("file", file);   // ← 반드시 "file" 키여야 함
+  const res = await api.post(join(ME_PATH, "profile-image"), form, {
+    headers: { "Content-Type": "multipart/form-data" }
+  });
   return res.data;
-};
+}
 
 export const getMyPosts = async (page = 0, size = 12) => {
   const res = await api.get(join(ME_PATH, "posts"), { params: { page, size } });
